e7c3013f117316782ec74664b0e7011787a1efe8
Dominique Broeglin <dominique.broeglin@gmail.com>
2024-11-24 19:20:27 +0100

    Updated test.py to use the module

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from rgbmatrix import graphics
 
 from PIL import Image, ImageDraw, ImageFont
+from pikaled import PikaLed
+
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
@@ -21,14 +23,10 @@ options.multiplexing = 4
 matrix = RGBMatrix(options = options)
 offset_canvas = matrix.CreateFrameCanvas()
 
-states = [random.choice(["miss", "hit", "unknown"]) for _ in range(options.chain_length * 2)]
 matrix.Clear()
 pikaled = PikaLed()
 
 while True:
-    for participant_nb in range(9):
-        for i, state, in enumerate(states):
-            pikaled.display_result(offset_canvas, state, participant_nb, i)
-
+    pikaled.update()
     offset_canvas = matrix.SwapOnVSync(offset_canvas)
     time.sleep(1)
